86fa75c6210996526145b02606d16cdcb0366520
Jörg Wagner <adalher@adalher.net>
2024-01-30 06:16:07 +0100

    add Exercise 7a

--- Part 1 - Fundamentals/Chapter 2 - Mathematical Problems/Exercise 7 - Roman Numbers.py
+++ Part 1 - Fundamentals/Chapter 2 - Mathematical Problems/Exercise 7 - Roman Numbers.py
@@ -41,10 +41,53 @@ def from_roman_numbers(roman_number: str) -> int:
     return number
 
 
+def to_roman_number(value: int) -> str:
+    roman = {1:{1:"I", 5:"V", 10:"X"}, 10:{1:"X", 5:"L", 10:"C"}, 100:{1:"C", 5:"D", 10:"M"}, 1000:{1:"M"}}
+    roman_number = []
+    index = 1
+    if not 0 < value < 4000:
+        return None
+    
+    def add_one(index: int, number: str, digit: int) -> (str, int):
+        number += roman[index][1]
+        digit -= 1
+        return number, digit
+    
+    def sub_one(index: int, number: str, digit: int) -> (str, int):
+        number = roman[index][1] + number
+        digit += 1
+        return number, digit
+    
+    while(value > 0):
+        digit = value % 10
+        roman_digit = ""
+        while(digit != 0):
+            if digit == -1:
+                roman_digit, digit = sub_one(index, roman_digit, digit)
+            elif digit <= 3:
+                roman_digit, digit = add_one(index, roman_digit, digit)
+            elif digit <= 8:
+                if not roman[index][5] in roman_digit:
+                    roman_digit = roman[index][5]
+                    digit -= 5
+            else:
+                if not roman[index][10] in roman_digit:
+                    roman_digit = roman[index][10]
+                    digit -= 10
+        
+        roman_number.append(roman_digit)
+        index *= 10
+        value //= 10
+
+    roman_number.reverse()
+    
+    return "".join(roman_number)
+
+
 if __name__ == "__main__":
-    values = ["XVII", "CDXLIV", "MCMLXXI", "MMXX", "IC", "XXXX", "MMMCMXCIX"]
+    values = ["XVII", "CDXLIV", "MCMLXXI", "MMXX", "MMMCMXCIX", "LXXVII"]
 
     for x in values:
         print(f"{x} -> ", end="")
-        print(from_roman_numbers(x))
-        print()
+        print(from_roman_numbers(x), end = " -> ")
+        print(to_roman_number(from_roman_numbers(x)))
